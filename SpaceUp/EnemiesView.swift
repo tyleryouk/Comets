cd0f306c96a81e12b2e363e7ce460ec3b9edee4c
Tyler Youk <tyleryouk@Tylers-iMac.home>
2016-05-11 19:03:19 -0400

    submit to app store 5/10

--- SpaceUp/EnemiesView.swift
+++ SpaceUp/EnemiesView.swift
@@ -8,22 +8,22 @@ class EnemiesView : ModalScrollView {
     var highestUserScore:Int?
     //var aLabel = ShadowLabelNode(fontNamed: FontName.RegularFont)
     
-    let firstImage = SpriteButtonNode(imageNamed: TextureFileName.kTedCruz)
-    let secondImage = SpriteButtonNode(imageNamed: TextureFileName.kHilaryClinton)
-    let thirdImage = SpriteButtonNode(imageNamed: TextureFileName.kRosie)
-    let fourthImage = SpriteButtonNode(imageNamed: TextureFileName.kMegyn)
-    let fifthImage = SpriteButtonNode(imageNamed: TextureFileName.kMacMiller)
-    let sixthImage = SpriteButtonNode(imageNamed: TextureFileName.kBeckyG)
-    let seventhImage = SpriteButtonNode(imageNamed: TextureFileName.kJohnOliver)
-    let eighthImage = SpriteButtonNode(imageNamed: TextureFileName.kSNL)
-    let ninethImage = SpriteButtonNode(imageNamed: TextureFileName.kHeidiCruz)
-    let tenthImage = SpriteButtonNode(imageNamed: TextureFileName.kBernieSanders)
-    let eleventhImage = SpriteButtonNode(imageNamed: TextureFileName.kBarackObama)
-    let twelvthImage = SpriteButtonNode(imageNamed: TextureFileName.kJebBush)
-    let thirteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kJohnKasich)
-    let fourteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kNBC)
-    let fifteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kFloRida)
-    let sixteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kMexico)
+    let firstImage = SpriteButtonNode(imageNamed: TextureFileName.kHilaryClinton)
+    let secondImage = SpriteButtonNode(imageNamed: TextureFileName.kTedCruz)
+    let thirdImage = SpriteButtonNode(imageNamed: TextureFileName.kBarackObama)
+    let fourthImage = SpriteButtonNode(imageNamed: TextureFileName.kJohnKasich)
+    let fifthImage = SpriteButtonNode(imageNamed: TextureFileName.kBernieSanders)
+    let sixthImage = SpriteButtonNode(imageNamed: TextureFileName.kEnrique)
+    let seventhImage = SpriteButtonNode(imageNamed: TextureFileName.kVladimirPutin)
+    let eighthImage = SpriteButtonNode(imageNamed: TextureFileName.kMegyn)
+    let ninethImage = SpriteButtonNode(imageNamed: TextureFileName.kgBush)
+    let tenthImage = SpriteButtonNode(imageNamed: TextureFileName.kJebBush)
+    let eleventhImage = SpriteButtonNode(imageNamed: TextureFileName.kMacMiller)
+    let twelvthImage = SpriteButtonNode(imageNamed: TextureFileName.kJohnOliver)
+    let thirteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kStephen)
+    let fourteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kjohnMccain)
+    let fifteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kMexico)
+    let sixteenthImage = SpriteButtonNode(imageNamed: TextureFileName.kislala)
    
     let spriteImagesArray: Array<SpriteButtonNode>
     
@@ -65,110 +65,110 @@ class EnemiesView : ModalScrollView {
         scrollingNode.addChild(firstImage)
         
         
-        let firstTextLabel:SKLabelNode = self.createNewLabelWithPoints(30, name: "Ted Cruz", xPosition: -range, yPosition: 160)
+        let firstTextLabel:SKLabelNode = self.createNewLabelWithPoints(30, name: "Hillary Clinton", xPosition: -range, yPosition: 160)
         scrollingNode.addChild(firstTextLabel)
     
         
         //0 - 1
-        let secondImage = self.createNewImageRequiredPoints(50, position: 1, xPosition: range, yPosition: 300)
+        let secondImage = self.createNewImageRequiredPoints(100, position: 1, xPosition: range, yPosition: 300)
         scrollingNode.addChild(secondImage)
-        let secondTextLabel = self.createNewLabelWithPoints(50, name:"Hilary Clinton", xPosition: range, yPosition: 160)
+        let secondTextLabel = self.createNewLabelWithPoints(100, name:"Ted Cruz", xPosition: range, yPosition: 160)
         scrollingNode.addChild(secondTextLabel)
         
         //1 - 0
-        let thirdImage = self.createNewImageRequiredPoints(75, position: 2, xPosition: -range, yPosition: 25)
+        let thirdImage = self.createNewImageRequiredPoints(180, position: 2, xPosition: -range, yPosition: 25)
         scrollingNode.addChild(thirdImage)
-        let thirdTextLabel = self.createNewLabelWithPoints(75, name:"Rosie O'Donnell", xPosition: -range, yPosition: -115)
+        let thirdTextLabel = self.createNewLabelWithPoints(180, name:"Barack Obama", xPosition: -range, yPosition: -115)
         scrollingNode.addChild(thirdTextLabel)
         
         //1 - 1
-        let fourthImage = self.createNewImageRequiredPoints(120, position: 3, xPosition: range, yPosition: 25)
+        let fourthImage = self.createNewImageRequiredPoints(280, position: 3, xPosition: range, yPosition: 25)
         scrollingNode.addChild(fourthImage)
-        let fourthTextLabel = self.createNewLabelWithPoints(120, name:"Megyn Kelly", xPosition: range, yPosition: -115)
+        let fourthTextLabel = self.createNewLabelWithPoints(280, name:"John Kasich", xPosition: range, yPosition: -115)
         scrollingNode.addChild(fourthTextLabel)
         
         //2 - 0
-        let fifthImage = self.createNewImageRequiredPoints(150, position: 4, xPosition: -range, yPosition: -250)
+        let fifthImage = self.createNewImageRequiredPoints(350, position: 4, xPosition: -range, yPosition: -250)
         scrollingNode.addChild(fifthImage)
-        let fifthTextLabel = self.createNewLabelWithPoints(150, name:"Mac Miller", xPosition: -range, yPosition: -390)
+        let fifthTextLabel = self.createNewLabelWithPoints(350, name:"Bernie Sanders", xPosition: -range, yPosition: -390)
         scrollingNode.addChild(fifthTextLabel)
         
         //2 - 1
-        let sixthImage = self.createNewImageRequiredPoints(170, position: 5, xPosition: range, yPosition: -250)
+        let sixthImage = self.createNewImageRequiredPoints(450, position: 5, xPosition: range, yPosition: -250)
         scrollingNode.addChild(sixthImage)
         
-        let sixthTextLabel = self.createNewLabelWithPoints(170, name:"Becky G", xPosition: range, yPosition: -390)
+        let sixthTextLabel = self.createNewLabelWithPoints(450, name:"Enrique Pena", xPosition: range, yPosition: -390)
         scrollingNode.addChild(sixthTextLabel)
         
         //3 - 0
-        let seventhImage = self.createNewImageRequiredPoints(220, position: 6, xPosition: -range, yPosition: -525)
+        let seventhImage = self.createNewImageRequiredPoints(550, position: 6, xPosition: -range, yPosition: -525)
         scrollingNode.addChild(seventhImage)
         
-        let seventhTextLabel = self.createNewLabelWithPoints(220, name:"John Oliver", xPosition: -range, yPosition: -665)
+        let seventhTextLabel = self.createNewLabelWithPoints(550, name:"Vladimir Putin", xPosition: -range, yPosition: -665)
         scrollingNode.addChild(seventhTextLabel)
         
         //3 - 1
-        let eightImage = self.createNewImageRequiredPoints(250, position: 7, xPosition: range, yPosition: -525)
+        let eightImage = self.createNewImageRequiredPoints(630, position: 7, xPosition: range, yPosition: -525)
         scrollingNode.addChild(eightImage)
         
-        let eightTextLabel = self.createNewLabelWithPoints(250, name:"SNL", xPosition: range - 10, yPosition: -665)
+        let eightTextLabel = self.createNewLabelWithPoints(630, name:"Megyn Kelly", xPosition: range - 10, yPosition: -665)
         scrollingNode.addChild(eightTextLabel)
         
        
         //4
-        let ninthImage = self.createNewImageRequiredPoints(280, position: 8, xPosition:-range, yPosition: -800)
+        let ninthImage = self.createNewImageRequiredPoints(730, position: 8, xPosition:-range, yPosition: -800)
         scrollingNode.addChild(ninthImage)
         
-        let ninthTextLabel = self.createNewLabelWithPoints(280, name:"Heidi Cruz", xPosition: -range, yPosition: -940)
+        let ninthTextLabel = self.createNewLabelWithPoints(730, name:"George Bush", xPosition: -range, yPosition: -940)
         scrollingNode.addChild(ninthTextLabel)
         
         //4 - 1
-        let tenthImage = self.createNewImageRequiredPoints(320, position: 9, xPosition: range, yPosition: -800)
+        let tenthImage = self.createNewImageRequiredPoints(900, position: 9, xPosition: range, yPosition: -800)
         scrollingNode.addChild(tenthImage)
         
-        let tenthTextLabel = self.createNewLabelWithPoints(320, name:"Bernie Sanders", xPosition: range, yPosition: -940)
+        let tenthTextLabel = self.createNewLabelWithPoints(900, name:"Jeb Bush", xPosition: range, yPosition: -940)
         scrollingNode.addChild(tenthTextLabel)
         
         //5
-        let eleventhImage = self.createNewImageRequiredPoints(350, position: 10, xPosition:-range, yPosition: -1075)
+        let eleventhImage = self.createNewImageRequiredPoints(1000, position: 10, xPosition:-range, yPosition: -1075)
         scrollingNode.addChild(eleventhImage)
         
-        let eleventhTextLabel = self.createNewLabelWithPoints(350, name:"Barack Obama", xPosition: -range, yPosition: -1215)
+        let eleventhTextLabel = self.createNewLabelWithPoints(1000, name:"Mac Miller", xPosition: -range, yPosition: -1215)
         scrollingNode.addChild(eleventhTextLabel)
         
         //5 - 1
-        let twelvthImage = self.createNewImageRequiredPoints(380, position: 11, xPosition: range, yPosition: -1075)
+        let twelvthImage = self.createNewImageRequiredPoints(1225, position: 11, xPosition: range, yPosition: -1075)
         scrollingNode.addChild(twelvthImage)
         
-        let twelvthTextLabel = self.createNewLabelWithPoints(380, name:"Jeb Bush", xPosition: range, yPosition: -1215)
+        let twelvthTextLabel = self.createNewLabelWithPoints(1225, name:"John Oliver", xPosition: range, yPosition: -1215)
         scrollingNode.addChild(twelvthTextLabel)
         
         //6
-        let thirteenthImage = self.createNewImageRequiredPoints(420, position: 12, xPosition:-range, yPosition: -1350)
+        let thirteenthImage = self.createNewImageRequiredPoints(1275, position: 12, xPosition:-range, yPosition: -1350)
         scrollingNode.addChild(thirteenthImage)
         
-        let thirteenthTextLabel = self.createNewLabelWithPoints(420, name:"John Kasich", xPosition: -range, yPosition: -1490)
+        let thirteenthTextLabel = self.createNewLabelWithPoints(1275, name:"Stephen Colbert", xPosition: -range, yPosition: -1490)
         scrollingNode.addChild(thirteenthTextLabel)
         
         //6 - 1
-        let fourteenthImage = self.createNewImageRequiredPoints(450, position: 13, xPosition: range, yPosition: -1350)
+        let fourteenthImage = self.createNewImageRequiredPoints(1350, position: 13, xPosition: range, yPosition: -1350)
         scrollingNode.addChild(fourteenthImage)
         
-        let fourteenthTextLabel = self.createNewLabelWithPoints(450, name:"NBC", xPosition: range, yPosition: -1490)
+        let fourteenthTextLabel = self.createNewLabelWithPoints(1350, name:"John Mccain", xPosition: range, yPosition: -1490)
         scrollingNode.addChild(fourteenthTextLabel)
         
         //7 - 1
-        let fifteenthImage = self.createNewImageRequiredPoints(470, position: 14, xPosition:-range, yPosition: -1625)
+        let fifteenthImage = self.createNewImageRequiredPoints(1500, position: 14, xPosition:-range, yPosition: -1625)
         scrollingNode.addChild(fifteenthImage)
         
-        let fifteenthTextLabel = self.createNewLabelWithPoints(470, name:"Flo Rida", xPosition: -range, yPosition: -1765)
+        let fifteenthTextLabel = self.createNewLabelWithPoints(1500, name:"Mexico", xPosition: -range, yPosition: -1765)
         scrollingNode.addChild(fifteenthTextLabel)
         
         //8 - 1
-        let sixteenthImage = self.createNewImageRequiredPoints(600, position: 15, xPosition: range, yPosition: -1625)
+        let sixteenthImage = self.createNewImageRequiredPoints(2000, position: 15, xPosition: range, yPosition: -1625)
         scrollingNode.addChild(sixteenthImage)
         
-        let sixteenthTextLabel = self.createNewLabelWithPoints(600, name:"Mexico", xPosition: range, yPosition: -1765)
+        let sixteenthTextLabel = self.createNewLabelWithPoints(2000, name:"ISIS", xPosition: range, yPosition: -1765)
         scrollingNode.addChild(sixteenthTextLabel)
         
     }
